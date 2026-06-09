b4273bc9a5f150e4b07e228bf120e920708a3e56
NukalaLalitha <66304238+NukalaLalitha@users.noreply.github.com>
2026-06-09 19:46:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
+     subnets = ["subnet-0a903910750d22999", "subnet-0450255907d464637"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
