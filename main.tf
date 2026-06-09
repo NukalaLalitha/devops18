4a936c494c8677f3950508b4ee91b5b5d17fd4fb
NukalaLalitha <66304238+NukalaLalitha@users.noreply.github.com>
2026-06-09 22:11:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_launch_template" "web_server_as" {
    
 
 
-  resource "aws_elb" "web_server_lb"{
+  resource "aws_elb" "web_server_lb" {
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-0a903910750d22999", "subnet-0450255907d464637"]
